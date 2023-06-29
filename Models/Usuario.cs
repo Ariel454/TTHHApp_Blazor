fe7bb79ff7f90c5e6dbafcab126464a912488712
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-06-29 09:07:13 -0500

    Tabla usuarios

---
+++ Models/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHHApp_Blazor.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+
+
+
+
+    public class MyArray
+    {
+        public string NOMBREUSUARIO { get; set; }
+        public string PERFIL { get; set; }
+        public string OBSERVACION { get; set; }
+        public int CODIGOPERFIL { get; set; }
+        public string ESTADO { get; set; }
+        public int COMPANIA { get; set; }
+        public int Emisor { get; set; }
+        public int Cargo { get; set; }
+        public string NOMBREEMISOR { get; set; }
+        public string NOMBRECOMPANIA { get; set; }
+        public string USUARIOCLIENTE { get; set; }
+        public string RucUsuario { get; set; }
+    }
+
+    public class Root
+    {
+        public List<MyArray> MyArray { get; set; }
+    }
+
+}

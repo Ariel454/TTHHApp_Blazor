59df0e5f787f62af8f98e5930c465a83dfcfc283
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-06-28 21:33:10 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TTHHApp_Blazor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TTHHApp_Blazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}

84c098cc451f2640ea830e4c37c2e0ebc37a7266
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-07-05 23:49:30 -0500

    Centro de costos

---
+++ Models/CentroCostos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHHApp_Blazor.Models
+{
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class CentroCostos
+    {
+        public int Codigo { get; set; }
+        public string NombreCentroCostos { get; set; }
+        public object Mensaje { get; set; }
+    }
+
+
+}

d9954ede1662139613e6fd211732074782598e56
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-07-12 22:46:09 -0500

    Implementacion Proxy para el trabajador y metodos de ingreso

---
+++ Models/DecimosProxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHHApp_Blazor.Models
+{
+    public class DecimosProxy
+    {
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+
+
+        public string ObtenerCalculoDecimo()
+        {
+            if (Codigo == "0")
+            {
+                return "Mensual";
+            }
+            else if (Codigo == "1")
+            {
+                return "Acumulada";
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+        }
+
+    }
+
+
+}

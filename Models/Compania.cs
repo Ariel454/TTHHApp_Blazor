00c6a648f104f1eab4cfe4a9626ea12ebe1c0a25
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-07-20 02:14:54 -0500

    Login

---
+++ Models/Compania.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHHApp_Blazor.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Compania
+    {
+        public int Codigo { get; set; }
+        public string NombreEmisor { get; set; }
+        public string Ruc { get; set; }
+    }
+
+
+}

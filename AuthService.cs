eef1f0cd92ad8d0b504afb1e7301ef4e6b2f6ca5
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-07-20 08:17:49 -0500

    Login

---
+++ AuthService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHHApp_Blazor
+{
+
+        public interface IAuthService
+        {
+            Task SetCompania(string nombreCompania);
+            string GetCompania();
+        }
+
+        public class AuthService : IAuthService
+        {
+            private string nombreCompania;
+
+            public Task SetCompania(string nombreCompania)
+            {
+                this.nombreCompania = nombreCompania;
+                return Task.CompletedTask;
+            }
+
+            public string GetCompania()
+            {
+                return nombreCompania;
+            }
+        }
+    
+}
